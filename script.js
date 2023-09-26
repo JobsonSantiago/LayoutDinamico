1a579b3978467bd28e486bb90d77299e331e3189
Jobson Santiago <126084728+JobsonSantiago@users.noreply.github.com>
2023-09-26 19:41:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// JavaScript demonstration
+
+//configurara tema escuro
+function doDemo(button) {
+
+    var square = document.getElementById("square");
+    square.style.backgroundColor = "#000000";
+    square.style.color = "white";
+
+    var x = document.getElementById("txtNome");
+    x.style.backgroundColor = "#1C1C1C";
+    x.style.color = "white";
+
+    var x = document.getElementById("txtTelefone");
+    x.style.backgroundColor = "#1C1C1C";
+    x.style.color = "white";
+
+    var x = document.getElementById("txtOrigem");
+    x.style.backgroundColor = "#1C1C1C";
+    x.style.color = "white";
+
+    var x = document.getElementById("txtDataContato");
+    x.style.backgroundColor = "#1C1C1C";
+    x.style.color = "white";
+
+    var x = document.getElementById("txtObservacao");
+    x.style.backgroundColor = "#1C1C1C";
+    x.style.color = "white";
+}
+
+//configurar tema claro
+function doDemo1(button) {
+
+  var square = document.getElementById("square");
+  square.style.backgroundColor = "white";
+  square.style.color = "black";
+
+  var x = document.getElementById("txtNome");
+    x.style.backgroundColor = "white";
+    x.style.color = "#000000";
+
+    var x = document.getElementById("txtTelefone");
+    x.style.backgroundColor = "white";
+    x.style.color = "#000000";
+
+    var x = document.getElementById("txtOrigem");
+    x.style.backgroundColor = "white";
+    x.style.color = "#000000";
+
+    var x = document.getElementById("txtDataContato");
+    x.style.backgroundColor = "white";
+    x.style.color = "#000000";
+
+    var x = document.getElementById("txtObservacao");
+    x.style.backgroundColor = "white";
+    x.style.color = "#000000";
+}
